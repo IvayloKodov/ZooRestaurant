509ca05b7c8d52583e0aead6247313e6c4445967
IvayloKodov <Ivailo_Kodov@abv.bg>
2017-05-10 08:29:20 +0300

    .

--- ZooRestaurant/Web/ZooRestaurant.Web/Startup.cs
+++ ZooRestaurant/Web/ZooRestaurant.Web/Startup.cs
@@ -4,11 +4,18 @@ using Owin;
 [assembly: OwinStartupAttribute(typeof(ZooRestaurant.Web.Startup))]
 namespace ZooRestaurant.Web
 {
+    using System.Data.Entity.Migrations;
+    using Data.Migrations;
+
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
             this.ConfigureAuth(app);
+
+            var configuration = new Configuration();
+            var migrator = new DbMigrator(configuration);
+            migrator.Update();
         }
     }
 }
