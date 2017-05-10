2d69b289c19b4e074c4de9a1d7b22c54f076989c
IvayloKodov <Ivailo_Kodov@abv.bg>
2017-05-10 10:46:06 +0300

    .

--- ZooRestaurant/Web/ZooRestaurant.Web.Common/Helpers/PathHelper.cs
+++ ZooRestaurant/Web/ZooRestaurant.Web.Common/Helpers/PathHelper.cs
@@ -3,16 +3,11 @@
     using System;
     using System.IO;
     using System.Reflection;
-    using System.Web;
-    using System.Web.Hosting;
 
     public static class PathHelper
     {
         public static string MapPath(string filePath,Assembly assembly)
         {
-            //if (HttpContext.Current != null)
-            //    return HostingEnvironment.MapPath(filePath);
-
             var absolutePath = new Uri(assembly.CodeBase).AbsolutePath;
             var directoryName = Path.GetDirectoryName(absolutePath);
             var path = Path.Combine(directoryName, filePath.TrimStart('~').Replace('/', '\\'));
